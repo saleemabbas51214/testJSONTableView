055dbd8b2a37d377d7e7784479a6300a9a255ddf
Saleem.Abbas <saleem.abbasfrauas@gmail.com>
2019-12-09 16:06:35 +0100

    Initial commit to setup skeleton or project

---
+++ testProjectEgym/testProjectEgym/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testProjectEgym
+//
+//  Created by MacBook on 09.12.19.
+//  Copyright © 2019 MacBook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
